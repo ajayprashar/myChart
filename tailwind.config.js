8d528d838c15b2be5ce22244edb555e14ff27b62
ajayprashar <ajay@aprashar.com>
2024-11-11 15:13:39 -0800

    Cleaned up another minor tailwind error...we are good to go!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    "./index.html"
+  ],
   theme: {
     extend: {},
   },
