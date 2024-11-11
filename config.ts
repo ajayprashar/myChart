1f6e736fc921f848b5885d530419951938df2dd6
ajayprashar <ajay@aprashar.com>
2024-11-11 15:12:09 -0800

    Tailwing install and cleanup

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const       CLIENT_ID = "468f238a-9aae-4aaf-b621-e205db69b01d"
+export const   FHIR_BASE_URL = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/"
+export const  SMART_AUTH_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize"
+export const SMART_TOKEN_URL = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
